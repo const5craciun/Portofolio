2680c0203d3e0a86a052f79914506d0e67f0ebb9
const5craciun <59504800+const5craciun@users.noreply.github.com>
2026-03-30 21:11:14 +0200

    changed file location

--- prediction_project/config/settings.py
+++ prediction_project/config/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predictor',
+    'predict',
 ]
 ROOT_URLCONF = 'config.urls'
 
